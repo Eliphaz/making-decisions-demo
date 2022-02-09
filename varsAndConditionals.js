d9e9eae90b7644dfa8010e89fd97b9c1ea3e5439
Eliphaz <eliahlander3@gmail.com>
2022-02-09 13:21:18 -0700

    added a for if and while loop

--- varsAndConditionals.js
+++ varsAndConditionals.js
@@ -5,7 +5,7 @@
 */ 
 
 let eowynAttack = 50;
-let witchKingAttack = 45;
+let witchKingAttack = 5;
 
 if(eowynAttack > witchKingAttack){
     console.log("Eowyn has a superior attack")
@@ -13,7 +13,7 @@ if(eowynAttack > witchKingAttack){
     console.log("They have a equal attack")
 } else{
     console.log("Witch King has a superior attack")
-}
+};
 
 let eowynHealth = 100;
 let eowynDefense = 0;
@@ -24,4 +24,26 @@ if (eowynHealth + eowynDefense >= witchKingAttack){
     console.log(`Eowyns health is now: ${eowynHealth}`)
 } else{
     console.log("Eowyn dies")
+};
+
+let coinLandsHeads = false;
+
+if (coinLandsHeads){
+    console.log("the coin landed on heads")
+}else{
+    console.log("the coin landed on tails")
+}
+
+// for(let i = 0; i < 5; i++) {
+//     if(eowynHealth > 0) {
+//         eowynHealth = eowynHealth - witchKingAttack;
+//         console.log(`Eowyn has ${eowynHealth} health left.`);
+//     } else {
+//         console.log(`Eowyn dead.`);
+//     };
+// };
+
+while(eowynHealth > 0){
+    eowynHealth -= (witchKingAttack - eowynDefense)
+    console.log(`Eowyn has ${eowynHealth} health left.`);
 }
